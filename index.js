b64e6ca3e2509d4ec33b6d75ab90088ba9080ecf
Macrophage001 <champend02@gmail.com>
2022-05-17 15:37:00 -0400

    - My server is set up and running!

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 5000
 
-
+app.get('/', (req, resp) => {
+    resp.send('Welcome to the Pokemon App!');
+});
 
 app.listen(port, () => console.log('Listening on port: ' + port));
