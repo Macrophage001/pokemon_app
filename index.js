d7e5755f33ce4e0a313e38226e9c68e0a12cf12b
Macrophage001 <champend02@gmail.com>
2022-05-17 15:39:16 -0400

    - Connected my database, can see json in the browser

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
+const pokemon = require('./models/pokemon');
+
 const app = express();
-const port = process.env.port || 5000
+const port = process.env.port || 5000;
 
 app.get('/', (req, resp) => {
     resp.send('Welcome to the Pokemon App!');
+});
+
+app.get('/pokemon', (req, resp) => {
+    resp.send(pokemon);
 });
 
 app.listen(port, () => console.log('Listening on port: ' + port));
